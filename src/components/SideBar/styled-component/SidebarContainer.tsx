626c5a5eea10c26b6328b58a63173076e90dabde
Frank Villachica <furapay@gmail.com>
2022-12-04 19:16:11 -0500

    Correccíon del responsive (Alto mínimo general de la página)

--- src/components/SideBar/styled-component/SidebarContainer.tsx
+++ src/components/SideBar/styled-component/SidebarContainer.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const SidebarContainer = styled.div`
   width: 270px;
-  height: 100vh;
+  min-height: var(--general-minHeight);
   background-color: #2061DE;
   filter: drop-shadow(5px 4px 4px rgba(0, 0, 0, 0.15));
 `
