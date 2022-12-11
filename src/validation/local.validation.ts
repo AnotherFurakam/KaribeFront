21a5a736b16ee937067b9af3e7eb41b1bdb21c33
Wilmer <wilmer.r2004@gmail.com>
2022-12-11 12:41:04 -0500

    arreglando los problems

---
+++ src/validation/local.validation.ts
@@ -0,0 +1,15 @@
+import * as Yup from "yup";
+import { ILocal } from "../models/local.interface";
+
+export const LocalFormSchema: Yup.SchemaOf<ILocal> = Yup.object()
+  .shape({
+    telefono: Yup.string().required("Debe llenar el campo teléfono"),
+    descripcion: Yup.string()
+      .max(500, "Solo se permite hasta 500 caracteres")
+      .required("Debe llenar el campo descripción"),
+    ruc: Yup.string().required("Debe llenar el campo ruc"),
+    departamento: Yup.string().required("Debe llenar el campo departamento"),
+    provincia: Yup.string().required("Debe llenar el campo provincia"),
+    distrito: Yup.string().required("Debe llenar el campo distrito"),
+  })
+  .optional();
