928ffa764a55724a54c8e96c9aabfaecccdd6951
Frank Villachica <furapay@gmail.com>
2022-12-11 11:29:01 -0500

    Quitando y corrigiendo imports de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom"
 import { AdminOutlet } from "./components/AdminOutlet"
-import { LoginNavbar } from "./components/LoginNavbar"
-import { Navbar } from "./components/Navbar"
 import ProductState from "./context/Producto/ProductoProvider"
 import { Empleados } from "./pages/AdminPages/Empleados"
-import { Estadisticas } from "./pages/AdminPages/Estadisticas"
 import { Historial } from "./pages/AdminPages/Historial"
 import { Home } from "./pages/AdminPages/Home"
 import { Locales } from "./pages/AdminPages/Locales"
 import { OpcionProducto } from "./pages/AdminPages/OpcionProducto"
 import { Pagos } from "./pages/AdminPages/Pagos"
 import { Productos } from "./pages/AdminPages/Productos"
-import { Reportes } from "./pages/AdminPages/Reportes"
 import { LoginPage } from "./pages/LoginPage"
 
 function App() {
